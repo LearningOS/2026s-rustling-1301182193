f2e942ed9fc0612b1e3b627781762fbefd72ba4f
吴勋 <wuxun@Macintosh.local>
2026-03-12 01:14:14 +0800

    50

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,17 +3,25 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
 }
 
+impl Clone for Point {
+    fn clone(&self) -> Point{
+        Point {
+            x : self.x,
+            y : self.y,
+        }
+    }
+}
+
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
+    let x: Option<Point> = y.clone();
+    match x {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
